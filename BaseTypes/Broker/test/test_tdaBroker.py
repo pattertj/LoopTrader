28d5d3b57186551031de0eb0a8bbe1901caf1056
Tyler <pattertj@gmail.com>
2021-04-04 22:47:04 +0100

    commit

--- BaseTypes/Broker/test/test_tdaBroker.py
+++ BaseTypes/Broker/test/test_tdaBroker.py
@@ -28,6 +28,13 @@ class TestTdaBroker(unittest.TestCase):
         self.assertIsNotNone(response)
         self.assertEqual(response.orderid, requestorderid)
 
+    # RUN AT YOUR OWN RISK, THIS COULD OPEN NEW POSITIONS ON YOUR ACCOUNT. YOU MAY NEED TO REVISE THE SYMBOL
+    # def test_place_order(self):
+    #     request = baseRR.PlaceOrderRequestMessage(price=.01, quantity=1, symbol='AAPL_040521P60')
+    #     response = self.func.place_order(request)
+
+    #     self.assertIsNotNone(response)
+
     def test_get_option_chain(self):
         request = baseRR.GetOptionChainRequestMessage(symbol='$SPX.X', contracttype='PUT', includequotes=True, optionrange='OTM', fromdate=dt.date.today().strftime("%Y-%m-%d"), todate=(dt.date.today() + dt.timedelta(days=4)).strftime("%Y-%m-%d"))
         response = self.func.get_option_chain(request)
