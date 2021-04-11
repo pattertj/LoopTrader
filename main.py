5b23eeacc603d0e72039d0dfc8a28024d6460ed2
Tyler <pattertj@gmail.com>
2021-04-11 09:29:22 +0100

    basic chat bot functionality

--- main.py
+++ main.py
@@ -4,22 +4,26 @@ import logging.config
 from BaseTypes.Broker.tdaBroker import TdaBroker
 from BaseTypes.Database.sqliteDatabase import SqliteDatabase
 from BaseTypes.Mediator.botMediator import Bot
-from BaseTypes.Strategy.cspByDeltaStrategy import CspByDeltaStrategy
+from BaseTypes.Notifier.telegramNotifier import TelegramNotifier
+from BaseTypes.Strategy.cspStrategy import CspStrategy
 
 # Create Logging
 logging.config.fileConfig("logConfig.ini", defaults={
                           'logfilename': 'autotrader.log'}, disable_existing_loggers=False)
 
 # Create our strategies
-cspstrat = CspByDeltaStrategy(strategy_name="CSP1")
-cspstrat1 = CspByDeltaStrategy(strategy_name="CSP2")
-cspstrat2 = CspByDeltaStrategy(strategy_name="CSP3")
+cspstrat = CspStrategy(strategy_name="CSP1")
+cspstrat1 = CspStrategy(strategy_name="CSP2")
+cspstrat2 = CspStrategy(strategy_name="CSP3")
 
 # Create our broker
 tdabroker = TdaBroker()
 
 # Create our local DB
 sqlitedb = SqliteDatabase()
+
+# Create our notifier
+telegram_bot = TelegramNotifier()
 
 # Create our Bot
 bot = Bot(broker=tdabroker, strategies=[cspstrat, cspstrat1, cspstrat2], database=sqlitedb)
