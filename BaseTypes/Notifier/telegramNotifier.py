40af763bd0a46a796b65fc6b6c7045e0a1afaab2
Tyler <pattertj@gmail.com>
2021-05-05 23:40:53 +0100

    cleanup

--- BaseTypes/Notifier/telegramNotifier.py
+++ BaseTypes/Notifier/telegramNotifier.py
@@ -1,3 +1,4 @@
+from BaseTypes.Mediator.botMediator import Bot
 import logging
 import re
 from os import getenv
@@ -31,7 +32,7 @@ class TelegramNotifier(Notifier, Component):
 
         # create the updater, that will automatically create also a dispatcher and a queue to make them dialoge
         self.updater = Updater(token, use_context=True)
-        dispatcher = Dispatcher(self.updater.dispatcher, self.updater.update_queue)
+        dispatcher = self.updater.dispatcher
 
         # add handlers for start and help commands
         dispatcher.add_handler(CommandHandler("start", self.start))
@@ -149,12 +150,13 @@ class TelegramNotifier(Notifier, Component):
 
         for order in account.orders:
             if order.status == 'OPEN' or order.status == 'QUEUED':
-                reply += " \r\n <code>- {}x {} {} @ ${}</code>".format(str(order.quantity), str(order.legs[0].instruction), str(order.legs[0].symbol), "{:,.2f}".format(order.price))
+                price = 0 if order.price is None else order.price
+                reply += " \r\n <code>- {}x {} {} @ ${}</code>".format(str(order.quantity), str(order.legs[0].instruction), str(order.legs[0].symbol), "{:,.2f}".format(price))
 
         return reply
 
     def button(self, update: Update, _: CallbackContext) -> None:
-        query = CallbackQuery(update.callback_query)
+        query = update.callback_query
 
         query.answer()
 
