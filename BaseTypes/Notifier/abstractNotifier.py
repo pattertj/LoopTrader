781e78497c960bdc85e16becedb2b2dea3b4b559
Tyler <pattertj@gmail.com>
2021-05-09 11:56:51 +0100

    missing docstring

--- BaseTypes/Notifier/abstractNotifier.py
+++ BaseTypes/Notifier/abstractNotifier.py
@@ -10,5 +10,6 @@ class Notifier(abc.ABC, Component):
 
     @abc.abstractmethod
     def send_notification(self, request: baseRR.SendNotificationRequestMessage) -> None:
+        '''Method to handle bot requests to push notifications'''
         raise NotImplementedError(
             "Each strategy must implement the 'do_something' method.")
