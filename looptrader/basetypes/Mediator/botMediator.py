06ef4fecd9bb7fc9e1792197fb10cfac0ed55c09
Tyler <pattertj@gmail.com>
2021-05-13 08:28:08 +0100

    initial

--- looptrader/basetypes/Mediator/botMediator.py
+++ looptrader/basetypes/Mediator/botMediator.py
@@ -3,14 +3,15 @@ import logging.config
 import time
 
 import attr
-import BaseTypes.Mediator.reqRespTypes as baseRR
-from BaseTypes.Broker.abstractBroker import Broker
-from BaseTypes.Database.abstractDatabase import Database
-from BaseTypes.Mediator.abstractMediator import Mediator
-from BaseTypes.Notifier.abstractnotifier import Notifier
-from BaseTypes.Strategy.abstractStrategy import Strategy
+import basetypes.Mediator.reqRespTypes as baseRR
 
-logger = logging.getLogger('autotrader')
+from looptrader.basetypes.Broker.abstractBroker import Broker
+from looptrader.basetypes.Database.abstractDatabase import Database
+from looptrader.basetypes.Mediator.abstractMediator import Mediator
+from looptrader.basetypes.Notifier.abstractnotifier import Notifier
+from looptrader.basetypes.Strategy.abstractStrategy import Strategy
+
+logger = logging.getLogger("autotrader")
 
 
 @attr.s(auto_attribs=True)
@@ -18,9 +19,18 @@ class Bot(Mediator):
     broker: Broker = attr.ib(validator=attr.validators.instance_of(Broker))
     notifier: Notifier = attr.ib(validator=attr.validators.instance_of(Notifier))
     database: Database = attr.ib(validator=attr.validators.instance_of(Database))
-    botloopfrequency: int = attr.ib(validator=attr.validators.instance_of(int), init=False)
-    killswitch: bool = attr.ib(default=False, validator=attr.validators.instance_of(bool), init=False)
-    strategies: list[Strategy] = attr.ib(validator=attr.validators.deep_iterable(member_validator=attr.validators.instance_of(Strategy), iterable_validator=attr.validators.instance_of(list)))
+    botloopfrequency: int = attr.ib(
+        validator=attr.validators.instance_of(int), init=False
+    )
+    killswitch: bool = attr.ib(
+        default=False, validator=attr.validators.instance_of(bool), init=False
+    )
+    strategies: list[Strategy] = attr.ib(
+        validator=attr.validators.deep_iterable(
+            member_validator=attr.validators.instance_of(Strategy),
+            iterable_validator=attr.validators.instance_of(list),
+        )
+    )
 
     def __attrs_post_init__(self):
         self.botloopfrequency = 30
@@ -44,12 +54,14 @@ class Bot(Mediator):
             # under = strategy.underlying
             strategy.mediator = self
 
-    def process_strategies(self) -> bool:
+    def process_strategies(self):
         # Get the current timestamp
         starttime = time.time()
 
         # If the loop is exited, send a notification
-        self.send_notification(baseRR.SendNotificationRequestMessage(message="Bot Started."))
+        self.send_notification(
+            baseRR.SendNotificationRequestMessage(message="Bot Started.")
+        )
 
         # While the kill switch is not enabled, loop through strategies
         while not self.killswitch:
@@ -61,31 +73,48 @@ class Bot(Mediator):
 
             # Sleep for the specified time.
             logger.info("Sleeping...")
-            time.sleep(self.botloopfrequency - ((time.time() - starttime) % self.botloopfrequency))
+            time.sleep(
+                self.botloopfrequency
+                - ((time.time() - starttime) % self.botloopfrequency)
+            )
 
         # If the loop is exited, send a notification
-        self.send_notification(baseRR.SendNotificationRequestMessage(message="Bot Terminated."))
+        self.send_notification(
+            baseRR.SendNotificationRequestMessage(message="Bot Terminated.")
+        )
 
-    def get_account(self, request: baseRR.GetAccountRequestMessage) -> baseRR.GetAccountResponseMessage:
+    def get_account(
+        self, request: baseRR.GetAccountRequestMessage
+    ) -> baseRR.GetAccountResponseMessage:
         return self.broker.get_account(request)
 
-    def place_order(self, request: baseRR.PlaceOrderRequestMessage) -> baseRR.PlaceOrderResponseMessage:
+    def place_order(
+        self, request: baseRR.PlaceOrderRequestMessage
+    ) -> baseRR.PlaceOrderResponseMessage:
         return self.broker.place_order(request)
 
-    def cancel_order(self, request: baseRR.CancelOrderRequestMessage) -> baseRR.CancelOrderResponseMessage:
+    def cancel_order(
+        self, request: baseRR.CancelOrderRequestMessage
+    ) -> baseRR.CancelOrderResponseMessage:
         return self.broker.cancel_order(request)
 
-    def get_order(self, request: baseRR.GetOrderRequestMessage) -> baseRR.GetOrderResponseMessage:
+    def get_order(
+        self, request: baseRR.GetOrderRequestMessage
+    ) -> baseRR.GetOrderResponseMessage:
         return self.broker.get_order(request)
 
-    def get_market_hours(self, request: baseRR.GetMarketHoursRequestMessage) -> baseRR.GetMarketHoursResponseMessage:
+    def get_market_hours(
+        self, request: baseRR.GetMarketHoursRequestMessage
+    ) -> baseRR.GetMarketHoursResponseMessage:
         return self.broker.get_market_hours(request)
 
-    def get_option_chain(self, request: baseRR.GetOptionChainRequestMessage) -> baseRR.GetOptionChainResponseMessage:
+    def get_option_chain(
+        self, request: baseRR.GetOptionChainRequestMessage
+    ) -> baseRR.GetOptionChainResponseMessage:
         return self.broker.get_option_chain(request)
 
     def send_notification(self, request: baseRR.SendNotificationRequestMessage) -> None:
         self.notifier.send_notification(request)
 
     def set_kill_switch(self, request: baseRR.SetKillSwitchRequestMessage) -> None:
-        self.killswitch = request
+        self.killswitch = request.kill_switch
--- looptrader/basetypes/Strategy/abstractStrategy.py
+++ looptrader/basetypes/Strategy/abstractStrategy.py
@@ -1,7 +1,8 @@
 import abc
 
 import attr
-from BaseTypes.Component.abstractComponent import Component
+
+from looptrader.basetypes.Component.abstractComponent import Component
 
 
 @attr.s(auto_attribs=True)
@@ -9,10 +10,12 @@ class Strategy(abc.ABC, Component):
     """
     The Strategy class provides the basic framework of a strategy that can be extended to many option trading strategies.
     """
+
     strategy_name: str = attr.ib(validator=attr.validators.instance_of(str))
     underlying: str = attr.ib(validator=attr.validators.instance_of(str))
 
     @abc.abstractmethod
-    def process_strategy(self) -> bool:
+    def process_strategy(self):
         raise NotImplementedError(
-            "Each strategy must implement the 'ProcessStrategy' method.")
+            "Each strategy must implement the 'ProcessStrategy' method."
+        )
