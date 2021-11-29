a20426bafc69679b098f915f65930463d93734d9
Tyler Patterson <pattertj@gmail.com>
2021-11-29 19:53:21 +0000

    fixes

--- looptrader/basetypes/Strategy/longsharesstrategy.py
+++ looptrader/basetypes/Strategy/longsharesstrategy.py
@@ -5,8 +5,8 @@ import math
 import time
 
 import attr
-import basetypes.Mediator.reqRespTypes as baseRR
 import basetypes.Mediator.baseModels as baseModels
+import basetypes.Mediator.reqRespTypes as baseRR
 from basetypes.Component.abstractComponent import Component
 from basetypes.Strategy.abstractStrategy import Strategy
 
